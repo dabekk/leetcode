95e49ad2e0f8fece6cf26d5d225b58db7e6135a3
Kamil Dabek <dabekk@tcd.ie>
2021-01-07 22:06:16 +0000

    Update linear solution.py

--- Container With Most Water/linear solution.py
+++ Container With Most Water/linear solution.py
@@ -3,7 +3,7 @@ class Solution:
         result = 0
         left_pos = 0
         right_pos = len(heights) - 1
-        for i in range(0, len(heights) - 1):
+        while(left_pos < right_pos):
             area = min(heights[left_pos], heights[right_pos]) * (right_pos - left_pos)
             result = max(result, area)
             if(heights[left_pos] < heights[right_pos]):
